513b2eba5e959f25da43444d8e5f7145f5248474
NathCad <nathalie.cadet.tb@gmail.com>
2024-09-09 19:57:56 +0200

    aria-label sur links suite retour rapport lighthouse

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,13 +5,21 @@ import { Github, LinkedIn } from "./images/Icons";
 import "./SocialLinks.scss";
 
 const socialLinks = [
-  { url: "www.linkedin.com/in/nathalie-cadet-9b037a309", icon: LinkedIn },
-  { url: "https://github.com/NathCad?tab=repositories", icon: Github },
+  {
+    url: "www.linkedin.com/in/nathalie-cadet-9b037a309",
+    icon: LinkedIn,
+    name: "LinkedIn",
+  },
+  {
+    url: "https://github.com/NathCad?tab=repositories",
+    icon: Github,
+    name: "GitHub",
+  },
 ];
 
-const SocialLink = ({ Icon, url }) => {
+const SocialLink = ({ Icon, url, name }) => {
   return (
-    <a className="social-link" href={url}>
+    <a className="social-link" href={url} aria-label={name}>
       <Icon />
     </a>
   );
@@ -20,8 +28,8 @@ const SocialLink = ({ Icon, url }) => {
 const SocialLinks = () => {
   return (
     <>
-      {socialLinks.map(({ url, icon }) => (
-        <SocialLink key={url} url={url} Icon={icon} />
+      {socialLinks.map(({ url, icon, name }) => (
+        <SocialLink key={url} url={url} Icon={icon} name={name} />
       ))}
     </>
   );
