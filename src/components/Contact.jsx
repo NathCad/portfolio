bfd3a20d7647027b86e95e9422fffcaed2ad7bdf
NathCad <nathalie.cadet.tb@gmail.com>
2024-09-09 19:45:17 +0200

    default vite config

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,27 +13,38 @@ const Contact = () => {
       </h2>
       <div className="content">
         <form>
-          <label className="paragraph-p1 thin">
-            Nom
-            <input type="text" placeholder="Votre nom" />
+          <label arclassName="paragraph-p1 thin">
+            <span className="required">Nom</span>
+            <input type="text" placeholder="Votre nom" required />
           </label>
           <label className="paragraph-p1 thin">
-            Email
-            <input type="email" placeholder="Votre email" />
+            <span className="required">Email</span>
+            <input type="email" placeholder="Votre email" required />
           </label>
           <label className="paragraph-p1 thin">
             Site web / Nom de l'entreprise
             <input
-              type="email"
+              type="text"
               placeholder="Adresse de votre site ou de votre entreprise"
             />
           </label>
           <label className="paragraph-p1 thin">
-            Message
-            <textarea rows="10" placeholder="Que puis-je pour vous ?" />
+            <span className="required">Message</span>
+            <textarea
+              rows="10"
+              placeholder="Que puis-je pour vous ?"
+              required
+            />
           </label>
+          <p className="paragraph-p1 thin">
+            les champs marqués d'un <span className="asterisk">*</span> sont
+            obligatoires
+          </p>
           <div className="form-inputs">
-            <button className="button-text">Envoyer<FontAwesomeIcon icon={faPaperPlane} /></button>
+            <button className="button-text">
+              Envoyer
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
             <SocialLinks />
           </div>
         </form>
@@ -43,7 +54,7 @@ const Contact = () => {
             écoute
           </h3>
           <p className="paragraph-p1">
-            Vous pouvez m'envoyer un petit message, pour toute demande comme par
+            Vous pouvez m'envoyer un petit message pour toute demande, comme par
             exemple une offre d'emploi ou un projet à développer.
           </p>
           <p className="paragraph-p1">
